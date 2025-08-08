5c13bea0205d165cb4e7b2bdbf5ee2974388085f
Marllon Ramos <marllonsteam@gmail.com>
2025-08-08 15:17:00 -0300

    DONE! Noise applied just on camera length

--- src/app/FullScene.tsx
+++ src/app/FullScene.tsx
@@ -1,55 +1,26 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Environment } from '@react-three/drei'
-import { EffectComposer, Bloom, Noise, ChromaticAberration } from '@react-three/postprocessing'
+import { EffectComposer, Noise } from '@react-three/postprocessing'
 import { BlendFunction } from 'postprocessing'
-import vertexShader from '../shaders/cube.vert.js'
-import fragmentShader from '../shaders/cube.frag.js'
-
-function MyModel() {
-  return (
-    <>
-      <mesh position={[-1.5, 0, 0]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <shaderMaterial
-          vertexShader={vertexShader}
-          fragmentShader={fragmentShader}
-        />
-      </mesh>
-      <mesh position={[1.5, 0, 0]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="red" />
-      </mesh>
-    </>
-  )
-}
 
 export function FullScene() {
   return (
     <Canvas 
+    gl={{ alpha: true }}
       camera={{ fov: 75, position: [0, 0, 5] }}
-      style={{ position: 'absolute', top: 0, left: 0 }}
+      style={{ position: 'absolute', top: 0, left: 0, width: '100vw', height: '100vh' }}
     >
-      {/* Fundo da cena */}
-      <mesh position={[0, 0, -10]}>
+      {/* Luz ambiente para iluminar a cena */}
+      <ambientLight intensity={1.0} />
+      
+      {/* Plane que ocupa toda a visão da câmera */}
+      <mesh position={[0, 0, 4]}>
         <planeGeometry args={[20, 20]} />
-        <meshStandardMaterial color="#fff" />
+        <meshStandardMaterial color="white" transparent opacity={0.1} />
       </mesh>
       
-      {/* Iluminação melhorada */}
-      <ambientLight intensity={1.0} />
-      <directionalLight position={[10, 10, 5]} intensity={2} />
-      <directionalLight position={[-10, -10, -5]} intensity={1} />
-      <pointLight position={[0, 10, 0]} intensity={1.5} />
-      <pointLight position={[0, -10, 0]} intensity={0.5} />
-      
-      {/* Environment para iluminação ambiente */}
-      <Environment preset="sunset" />
-      
-      <MyModel />
-      <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} />
       {/* Post-processing global para toda a cena 3D */}
       <EffectComposer>
-        <Noise premultiply blendFunction={BlendFunction.NORMAL} opacity={0.2} />
+        <Noise premultiply blendFunction={BlendFunction.NORMAL} opacity={1.0} />
       </EffectComposer>
     </Canvas>
   )
